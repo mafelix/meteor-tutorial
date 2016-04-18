09511e2a2da1ade2e041a45eb09d708a55342972
felix <felixmdev@gmail.com>
2016-04-17 22:39:59 -0700

    query calls to mongo db was moved to server based

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -37,10 +37,7 @@ Template.body.events({
     const text = target.text.value;
 
     // Insert a task into the collection
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
+    Meteor.call('tasks.insert', text);
 
     // Clear form
     target.text.value = '';
